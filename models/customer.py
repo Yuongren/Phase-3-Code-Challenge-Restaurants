49ba1ecc12ba221a35f70f28fc84bd64133c5424
gitYoungren <youngrengitonga@gmail.com>
2023-12-21 08:10:48 +0300

    coed added

--- models/customer.py
+++ models/customer.py
@@ -1,2 +1,24 @@
 class Customer:
-    pass
+    all = []
+
+
+    def __init__(self, name, restaurant):
+        self.name = name
+        self.restaurant = restaurant
+        Customer.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self.name = name
+
+    @property
+    def restaurant(self):
+        self.restaurant
+
+    @classmethod
+    def all(cls):
+        return cls.all
