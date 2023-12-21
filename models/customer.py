df5e606b855bb6da79d5b0bda47925048f100e3a
gitYoungren <youngrengitonga@gmail.com>
2023-12-21 08:39:06 +0300

    coed changed in customer.py

--- models/customer.py
+++ models/customer.py
@@ -2,23 +2,30 @@ class Customer:
     all = []
 
 
-    def __init__(self, name, restaurant):
-        self.name = name
-        self.restaurant = restaurant
-        Customer.all.append(self)
+    def __init__(self, given_name, family_name):
+        self.given_name = given_name
+        self.family_name = family_name
+        Customer.all_customers.append(self)
 
     @property
-    def name(self):
-        return self._name
+    def given_name(self):
+        return self._given_name
 
-    @name.setter
-    def name(self, name):
-        self.name = name
+    @given_name.setter
+    def name(self, new_name):
+        self._given_name = new_name
 
     @property
-    def restaurant(self):
-        self.restaurant
+    def family_name(self):
+        return self._family_name
+
+    @family_name.setter
+    def family_name(self, new_family_name):
+        self._family_name = new_family_name
+
+    def full_name(self):
+        return f"{self.given_name} {self.family_name}"
 
     @classmethod
     def all(cls):
-        return cls.all
+        return cls.all_customers
