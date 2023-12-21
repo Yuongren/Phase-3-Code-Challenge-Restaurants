873b5932738a628832a3c5b8e7e31947b588aacc
gitYoungren <youngrengitonga@gmail.com>
2023-12-21 08:55:14 +0300

    coed added in restaurant.py

--- models/restaurant.py
+++ models/restaurant.py
@@ -1,2 +1,9 @@
 class Restaurant:
-    pass
+    all_restaurants = []
+    def __init__(self, name):
+        self.name = name
+        Restaurant.all_restaurants.append(self)
+
+    @property
+    def name(self):
+        return self.name
